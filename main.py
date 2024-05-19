6f9d01c11b0eacae6c21e7688119c2a676cf48d1
Debangan-MishraIIIT <debangan.mishra@students.iiit.ac.in>
2024-05-19 15:17:48 +0530

    added geometric data conversion in src

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from src.config import args
 from src.dataset import load_dataset
 from src.poison_methods import attack
 from src.model_utils import train_model
-from src.unlearn_methods import get_unlearn_method, UnlearnMethod
+from src.unlearn_methods import get_unlearn_method
 
 
 if __name__ == "__main__":
@@ -12,8 +12,11 @@ if __name__ == "__main__":
         model, dataset, attack_type=args.attack
     )  # attack returns the poisoned adj and features
 
-    ## add unlearning here
-    method: UnlearnMethod = get_unlearn_method(
-        name="projector",
-    )  # kwargs to be defined, see function definition
-    method.unlearn()
+    print(poisoned_adj.shape)
+    print(poisoned_x.shape)
+    
+    # ## add unlearning here
+    # method: UnlearnMethod = get_unlearn_method(
+    #     name="projector",
+    # )  # kwargs to be defined, see function definition
+    # method.unlearn()
--- src/unlearn_methods.py
+++ src/unlearn_methods.py
@@ -4,6 +4,7 @@ import numpy as np
 import torch
 from unlearn.MEGU import MEGU
 from unlearn.Projector import Projector
+from torch_geometric.data import Data
 
 def get_unlearn_method(name, **kwargs):
     if name == "projector":
@@ -12,3 +13,15 @@ def get_unlearn_method(name, **kwargs):
         return MEGU(**kwargs)
     else:
         raise ValueError("Unlearn method not found")
+
+def make_geometric_data(poisoned_adj, poisoned_x, dataset):
+    num_nodes_added= poisoned_adj.shape[0]-dataset.adj.shape[0]
+    new_labels= torch.zeros(num_nodes_added)
+    poisoned_labels= torch.hstack((dataset.labels, new_labels))
+    poisoned_features= torch.vstack((dataset.features, poisoned_x))
+    data= Data(x=poisoned_features, edge_index=poisoned_adj, y=poisoned_labels)
+
+    #Adjacency matrix for grb compatible can be obtained by data.edge_index.toarray()
+    #for MEGU code, required to have data.num_classes= dataset.num_classes
+    data.num_classes= dataset.num_classes
+    return data
