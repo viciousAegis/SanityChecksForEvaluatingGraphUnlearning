f1872aa81a68be3277871a197ce231e7feb922be
Sumitkk10 <messikumar66@gmail.com>
2024-06-08 11:17:02 +0530

    trying graph training

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from gub.config import args
 from gub.models import load_model
 from gub.unlearn import init_unlearn_algo
 from gub.train import init_trainer
+from gub.train.graph_trainer import GCN
+from torch_geometric.loader import DataLoader
 
 seed = 1235
 torch.manual_seed(seed)
@@ -22,6 +24,48 @@ if __name__ == "__main__":
         hidden_features=args.hidden_features,
         n_layers=args.n_layers,
     )
+    
+    # dataset = dataset.shuffle()
+
+    # train_dataset = dataset[:891]
+    # test_dataset = dataset[891:]
+
+    # print(f'Number of training graphs: {len(train_dataset)}')
+    # print(f'Number of test graphs: {len(test_dataset)}')
+
+    # train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+    # test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
+
+    # model = GCN(dataset=dataset, hidden_channels=64)
+    # optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+    # criterion = torch.nn.CrossEntropyLoss()
+
+    # def train():
+    #     model.train()
+
+    #     for data in train_loader:  # Iterate in batches over the training dataset.
+    #         out = model(data.x, data.edge_index, data.batch)  # Perform a single forward pass.
+    #         loss = criterion(out, data.y)  # Compute the loss.
+    #         loss.backward()  # Derive gradients.
+    #         optimizer.step()  # Update parameters based on gradients.
+    #         optimizer.zero_grad()  # Clear gradients.
+
+    # def test(loader):
+    #     model.eval()
+
+    #     correct = 0
+    #     for data in loader:  # Iterate in batches over the training/test dataset.
+    #         out = model(data.x, data.edge_index, data.batch)  
+    #         pred = out.argmax(dim=1)  # Use the class with highest probability.
+    #         correct += int((pred == data.y).sum())  # Check against ground-truth labels.
+    #     return correct / len(loader.dataset)  # Derive ratio of correct predictions.
+
+
+    # for epoch in range(1, 171):
+    #     train()
+    #     train_acc = test(train_loader)
+    #     test_acc = test(test_loader)
+    #     print(f'Epoch: {epoch:03d}, Train Acc: {train_acc:.4f}, Test Acc: {test_acc:.4f}')
 
     trainer = init_trainer(
         task_level="node",
